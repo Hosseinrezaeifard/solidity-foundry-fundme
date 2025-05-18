aa9db0322eae579b3bc3ec5019272309ab903aec
HosseinRezaeiFard <huseinrezaeif99@gmail.com>
2025-05-18 14:36:43 -0700

    Writing more tests & a little bit of refactoring

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -10,11 +10,11 @@ error FundMe__NotOwner();
 contract FundMe {
     using PriceConverter for uint256;
 
-    mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
+    mapping (address funder => uint256 amountFunded) private s_addressToAmountFunded;
+    address[] private s_funders;
     
     address public immutable i_owner;
     uint256 public constant MIN_USD = 5e18;
-    address[] public funders;
     AggregatorV3Interface private s_priceFeed;
 
     constructor(address priceFeed) {
@@ -24,23 +24,19 @@ contract FundMe {
 
     function fund() public payable {
         require(msg.value.getConversionRate(s_priceFeed) > MIN_USD, "Didn't sent enough ETH");
-        funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+        s_addressToAmountFunded[msg.sender] += msg.value;
     }
 
     function withdraw() public onlyOwner {
-        for (uint256 i = 0; i < funders.length; i++) {
-            address funder = funders[i];
-            addressToAmountFunded[funder] = 0;
+        for (uint256 i = 0; i < s_funders.length; i++) {
+            address funder = s_funders[i];
+            s_addressToAmountFunded[funder] = 0;
         }
-        funders = new address[](0);
+        s_funders = new address[](0);
         (bool callSuccess, ) = payable (msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Send Failed!");
         revert();
-    }
-
-    function getVersion() public view returns (uint256) {
-        return s_priceFeed.version();
     }
 
     modifier onlyOwner() {
@@ -51,5 +47,34 @@ contract FundMe {
     receive() external payable { fund(); }
 
     fallback() external payable { fund(); }
+
+     /**
+     * Getter Functions
+     */
+
+    /**
+     * @notice Gets the amount that an address has funded
+     *  @param fundingAddress the address of the funder
+     *  @return the amount funded
+     */
+    function getAddressToAmountFunded(address fundingAddress) public view returns (uint256) {
+        return s_addressToAmountFunded[fundingAddress];
+    }
+
+    function getVersion() public view returns (uint256) {
+        return s_priceFeed.version();
+    }
+
+    function getFunder(uint256 index) public view returns (address) {
+        return s_funders[index];
+    }
+
+    function getOwner() public view returns (address) {
+        return i_owner;
+    }
+
+    function getPriceFeed() public view returns (AggregatorV3Interface) {
+        return s_priceFeed;
+    }
     
 }
